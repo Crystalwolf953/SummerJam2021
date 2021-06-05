5ac85fe3ee0d0beb9a40a912c2639b3ba40f94cf
Elliott-Schultz <elliottbschultz@gmail.com>
2021-06-04 22:18:15 -0400

    Refactor names for easier understanding

--- SummerGameJam/Assets/Scripts/Rotation.cs
+++ SummerGameJam/Assets/Scripts/Rotation.cs
@@ -8,8 +8,8 @@ public class Rotation : MonoBehaviour
     public float Rotation_Friction; //The smaller the value, the more Friction there is. [Keep this at 1 unless you know what you are doing].
     public float Rotation_Smoothness; //Believe it or not, adjusting this before anything else is the best way to go.
 
-    public float horizontalRotationRestrict;
-    public float verticalRotationRestrict;
+    public float horizontalRotationRestrictAngle;
+    public float verticalRotationRestrictAngle;
 
     private float horizontalRotation;
     private float verticalRotation;
@@ -21,11 +21,11 @@ public class Rotation : MonoBehaviour
     {
         // Get the input for rotation from the horizontal axis
         horizontalRotation += Input.GetAxis("Horizontal") * Rotation_Speed * Rotation_Friction;
-        horizontalRotation = Mathf.Clamp(horizontalRotation, -horizontalRotationRestrict, horizontalRotationRestrict);
+        horizontalRotation = Mathf.Clamp(horizontalRotation, -horizontalRotationRestrictAngle, horizontalRotationRestrictAngle);
 
         // Get the input for rotation from the vertical axis
         verticalRotation += Input.GetAxis("Vertical") * Rotation_Speed * Rotation_Friction;
-        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationRestrict, verticalRotationRestrict);
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationRestrictAngle, verticalRotationRestrictAngle);
 
         // Get the current rotation of the object
         currentRotation = transform.rotation;
